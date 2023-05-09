0c5b014c782a73fd38f501a1992b3015ca14afde
Nataly-Aketch <nataliebeckie@gmail.com>
2023-05-09 15:45:13 +0300

    The console

---
+++ console.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""this program contains the entry point of the command interpreter"""
+from models.base_model import BaseModel
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """command processor"""
+    prompt = '(hbnb) '
+
+    def do_EOF(self, line):
+        """End of File to exit the program"""
+        return True
+
+    def do_quit(self, line):
+        """Quit command to exit the program"""
+        exit()
+
+    def do_create(self, line):
+        """Creates a new instance of BaseModel, saves it (to the JSON file) and prints the id"""
+        if not line:
+            print("** class name missing **")
+        elif line != 'BaseModel':
+            print("** class doesn't exist **")
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
