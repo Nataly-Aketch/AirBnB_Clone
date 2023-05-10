6b2963f6bc8bc7de28f041ccd4c259bf7eb2e655
Nataly-Aketch <nataliebeckie@gmail.com>
2023-05-10 09:59:39 +0300

    Console

--- console.py
+++ console.py
@@ -3,6 +3,7 @@
 from models.base_model import BaseModel
 import cmd
 
+
 class HBNBCommand(cmd.Cmd):
     """command processor"""
     prompt = '(hbnb) '
@@ -16,11 +17,21 @@ class HBNBCommand(cmd.Cmd):
         exit()
 
     def do_create(self, line):
-        """Creates a new instance of BaseModel, saves it (to the JSON file) and prints the id"""
+        """Creates a new instance of BaseModel, saves it (to the JSON file)
+        and prints the id"""
         if not line:
             print("** class name missing **")
         elif line != 'BaseModel':
             print("** class doesn't exist **")
+        else:
+            new = BaseModel()
+            print(new.id)
+
+    def do_show(self, line):
+        """Prints the string representation of an instance based
+        on the class name and id"""
+        new_line = line.split()
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
