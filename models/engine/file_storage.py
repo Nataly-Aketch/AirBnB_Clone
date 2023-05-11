4aab9e1610d681f96e401be8518458b1a95ad934
Ronnie Leon <ronleon76@gmail.com>
2023-05-11 14:22:28 +0000

    fixed issue reloading JSON objects from file

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """defines FileStorage class"""
-import json
+import json, os
+from models.base_model import BaseModel
 
 
 class FileStorage():
@@ -10,26 +11,30 @@ class FileStorage():
 
     def all(self):
         """All Objects"""
+        self.reload()
         return FileStorage.__objects
 
     def new(self, obj):
-        """New Objects"""
+        """sets the object instance to __objects with key <obj class name>.id"""
         key = "{}.{}".format(type(obj).__name__, obj.id)
         FileStorage.__objects[key] = obj
 
     def save(self):
-        """Save an object to the file"""
-        dictionary = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
+        """Serializes the python objects to the JSON file class.__file_path"""
         with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
+            dictionary = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
             json.dump(dictionary, f)
 
     def reload(self):
         """Reload JSON objects in file to Python objects"""
-        try:
-            with open(FileStorage.__file_path, "r") as f:
-                obj_dict = load(f)
+        if os.path.isfile(FileStorage.__file_path):
+            with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
+                obj_dict = json.load(f)
+                print(obj_dict)
+                loaded_objects = {}
                 for k, v in obj_dict.items():
-                    cls_name = v['__class__']
-                    obj_dict[k] = cls_name(**v)
-        except Exception as e:
-            pass
+                    cls_name = globals()[v['__class__']]
+                    loaded_objects[k] = cls_name(**v)
+                FileStorage.__objects = loaded_objects
+        else:
+            return
