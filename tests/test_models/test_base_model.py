ccd61e99a874806e7c9b7f11c904e9e26a30ef11
Nataly-Aketch <nataliebeckie@gmail.com>
2023-05-12 19:31:06 +0300

    More tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,26 +1,44 @@
 #!/usr/bin/python3
 """Unit Testing for Base Model"""
+from contextlib import redirect_stdout
+from datetime import datetime
+from io import StringIO
+import io
+from models.base_model import BaseModel
 import unittest
 import time
-from models.base_model import BaseModel
 
-class TestBase(unittest.TestCase):
+
+class TestInstantation(unittest.TestCase):
     """Test the Base Model Class"""
     def test_instantiation(self):
-        """Tests instantiation of the BaseModel class"""
         my_model = BaseModel()
-        self.assertEqual(str(type(my_model)), "<class 'models.base_model.BaseModel'>")
         self.assertIsInstance(my_model, BaseModel)
-        self.assertTrue(issubclass(type(my_model), BaseModel))
-    
-    def test_to_dict(self):
-        """Tests to_dict() method"""
+
+    def test_id_type(self):
         my_model = BaseModel()
-        my_model.name = "My First Model"
-        my_model.my_number = 89
-        my_model_json = my_model.to_dict()
-        my_new_model = BaseModel(**my_model_json)
-        # self.assertEqual(my_new_model.to_dict(), my_model.to_dict())
+        self.assertEqual(type(my_model.id), str)
+
+    def test_uuid(self):
+        my_model = BaseModel()
+        other_model = BaseModel()
+        self.assertNotEqual(my_model.id, other_model.id)
+
+    def test_datetime(self):
+        my_model = BaseModel()
+        self.assertNotEqual(my_model.created_at, datetime.now())
+
+    def test_kwargs(self):
+        my_model = BaseModel(name="first")
+        self.assertTrue(hasattr(my_model, 'name'))
+
+
+class TestMethods(unittest.TestCase):
+    """Tests public instance methods of basemodel"""
+    def test_to_dict_type(self):
+        my_model = BaseModel()
+        my_model_dict = my_model.to_dict()
+        self.assertEqual(type(my_model_dict), dict)
 
     def test_save(self):
         """Tests the save() method"""
@@ -31,7 +49,13 @@ class TestBase(unittest.TestCase):
         diff = my_model.updated_at - date_now
         self.assertTrue(abs(diff.total_seconds()) < 0.01)
 
-
+    def test_str(self):
+        model = BaseModel()
+        expected = "[{}] ({}) {}\n".format(type(model).__name__, model.id,
+                                           model.__dict__)
+        with io.StringIO() as buf, redirect_stdout(buf):
+            print(model)
+            self.assertEqual(buf.getvalue(), expected)
 
 
 if __name__ == "__main__":
